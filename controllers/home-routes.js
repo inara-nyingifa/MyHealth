e7b0104d9cda05f1d06dcd59b75df0263269e218
LAgrl <lizethariasc14@gmail.com>
2021-07-27 21:52:03 -0700

    display homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,11 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    
-        res.render('homepage')
-  });
+  res.render('homepage');
+});
 
-  router.get('/signup', (req, res) => {
-    res.render('signup');
-  });
 module.exports = router;
