cbe58f37fb6ef0c68b87fcd56b4fb58e05e111d0
Inara Nyingifa <inarasnyingifa@gmail.com>
2021-07-27 21:18:23 -0700

    home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    
+        res.render('homepage')
+  });
+
+  router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,31 @@ const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require("path");
 
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3005;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: 'bigbluedog',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
