8bc42a3447a5563c700db0f5da42eda2fac95d27
Inara Nyingifa <inarasnyingifa@gmail.com>
2021-07-27 19:43:29 -0700

    added login routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET /api/users
+// GET 
 router.get('/', (req, res) => {
     // Access our User model
     User.findAll({
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
       });
   });
 
-// GET /api/users/1
+
 router.get('/:id', (req, res) => {
     User.findOne({
       attributes: { exclude: ['password'] },
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
       });
   });
 
-// POST /api/users
+// POST 
 router.post('/', (req, res) => {
     User.create({
       account_type: req.body.account_type,
@@ -49,13 +49,56 @@ router.post('/', (req, res) => {
       });
   });
 
-// PUT /api/users/1
-router.put('/:id', (req, res) => {
-    User.update(req.body, {
-      where: {
-        id: req.params.id
+    router.post('/login', (req, res) => {
+      
+      User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+          res.status(400).json({ message: 'Incorrect password!' });
+          return;
+        }
+        req.session.save(() => {
+          // session variables
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.twitter = dbUserData.twitter;
+          req.session.github = dbUserData.github;
+          req.session.loggedIn = true;
+  
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+      });
+    });
+  //logout
+    router.post('/logout', (req, res) => {
+      if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
       }
-    })
+      else {
+        res.status(404).end();
+      }
+    });
+
+// PUT 
+router.put('/:id', (req, res) => {
+  individualHooks: true,
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
       .then(dbUserData => {
         if (!dbUserData[0]) {
           res.status(404).json({ message: 'No user found with this id' });
@@ -91,4 +134,3 @@ router.delete('/:id', (req, res) => {
   
 
 module.exports = router;
-//added just to push on new branch
