9e944426e943257b2745f607c3c6e6f318cb645a
LAgrl <lizethariasc14@gmail.com>
2021-08-01 21:16:30 -0700

    server changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const controllers = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require("path");
 
+const helpers = require('./utils/helpers');
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
